0a4f3382a827cd8ce43c6702e57b56dde5860bea
Everton da Silva Correa <evertoncorrea95@gmail.com>
2022-08-24 19:20:15 -0300

    variaveis exibidas

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let email = "evertoncorrea95@gmail.com"
 let profissao = "analista contábil"
 let idade = 27
 let lugarSonho = "Patagônia"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade, "anos")
+console.log(lugarSonho)
