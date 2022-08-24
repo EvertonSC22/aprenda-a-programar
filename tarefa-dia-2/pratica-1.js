218b714408734b35ce1245cef58bbb1205df3c8b
Everton da Silva Correa <evertoncorrea95@gmail.com>
2022-08-24 19:40:04 -0300

    usando variaveis e console.log com template strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,8 +3,14 @@ let email = "evertoncorrea95@gmail.com"
 let profissao = "analista contábil"
 let idade = 27
 let lugarSonho = "Patagônia"
+//variáveis declaradas
+
 console.log(nome)
 console.log(email)
 console.log(profissao)
 console.log(idade, "anos")
 console.log(lugarSonho)
+// exibindo variáveis individualmente
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarSonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+// desafio
